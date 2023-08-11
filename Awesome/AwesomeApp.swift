ecd0b524897e0aa3761a09b40cd5849a1876597c
tungthncc <128009347+tungthncc@users.noreply.github.com>
2023-08-11 00:43:47 -0700

    Initial Commit

---
+++ Awesome/AwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AwesomeApp.swift
+//  Awesome
+//
+//  Created by Y Le Van on 11/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct AwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Awesome/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Awesome
+//
+//  Created by Y Le Van on 11/08/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
